8180a091e707c96ecdab9bd11a5f02518a1f5d52
Kumar Yuvraj Choudhary <yuvrajchoudhary69777@gmail.com>
2026-09-24 23:15:52 +0530

    Time: 5 ms (27.53%), Space: 263.3 MB (43.91%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
