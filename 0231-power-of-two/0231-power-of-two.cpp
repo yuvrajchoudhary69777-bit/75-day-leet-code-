35ea845efcfde30049aa0acd31d7a92756281a95
Kumar Yuvraj Choudhary <yuvrajchoudhary69777@gmail.com>
2026-09-24 23:12:08 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.6%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
