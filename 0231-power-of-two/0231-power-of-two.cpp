7f8ffa35fa0f7c9b7ca4e9f6dedce774728f5510
Kumar Yuvraj Choudhary <yuvrajchoudhary69777@gmail.com>
2026-09-26 15:57:22 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.28%) - LeetHub - Time: O(log n), Space: O(log n)

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
-        if(n==0) return false; 
-        while(n%2==0){
-            n/=2; 
-        }
-         return n==1;
+        //base case 
+    
+        if (n<=0) return false ; 
+        if (n==1) return true ; 
+        if (n%2==1) return false ;
+         
+        return isPowerOfTwo(n/2);
     }
 };
 
